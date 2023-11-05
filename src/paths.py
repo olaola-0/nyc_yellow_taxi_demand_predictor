483a4cea70fcbc81f11052b22ec990d22c65f4c5
olaola-0 <isholamyson@gmail.com>
2023-11-05 23:29:37 +0100

    added models directory to path.py

--- src/paths.py
+++ src/paths.py
@@ -9,6 +9,8 @@ DATA_DIR = PARENT_DIR / "data"
 RAW_DATA_DIR = PARENT_DIR / "data" / "raw"
 TRANSFORMED_DATA_DIR = PARENT_DIR / "data" / "transformed"
 
+MODELS_DIR = PARENT_DIR / 'models'
+
 # Check if the general data directory exists, if not, create it
 if not Path(DATA_DIR).exists():
     os.mkdir(DATA_DIR)
@@ -20,3 +22,7 @@ if not Path(RAW_DATA_DIR).exists():
 # Check if the transformed data directory exists, if not, create it
 if not Path(TRANSFORMED_DATA_DIR).exists():
     os.mkdir(TRANSFORMED_DATA_DIR)
+
+# Check if the models directory exists, if not, create it
+if not Path(MODELS_DIR).exists():
+    os.mkdir(MODELS_DIR)
