31d943bc08e72eaa1daa2353f2664cb98c0515f1
olaola-0 <isholamyson@gmail.com>
2023-11-07 02:21:36 +0100

    removed feature view & group metadata

--- src/config.py
+++ src/config.py
@@ -2,35 +2,33 @@ import os
 from dotenv import load_dotenv
 
 from src.paths import PARENT_DIR
-from src.feature_store_api import FeatureGroupConfig, FeatureViewConfig
 
-#load key-value pairs from .env file located in the parent directory
-load_dotenv(PARENT_DIR/ '.env')
+# Load key-value pairs from .env file located in the parent directory
+load_dotenv(PARENT_DIR / '.env')
 
+# Project name for Hopsworks
 HOPSWORKS_PROJECT_NAME = 'NYC_YELLOW_TAXI'
+# Retrieve the API key for Hopsworks, raise an exception if it is not found
 try:
     HOPSWORKS_API_KEY = os.environ['HOPSWORKS_API_KEY']
-except:
+except KeyError:
     raise Exception('Create a .env file in the project root with the HOPSWORKS_API_KEY')
 
+# Feature group name and version
 FEATURE_GROUP_NAME = 'time_series_hourly_feature_group'
 FEATURE_GROUP_VERSION = 1
-FEATURE_GROUP_METADATA = FeatureGroupConfig(
-    name='time_series_hourly_feature_group',
-    version=1,
-    description='Feature group with hourly time-series data of historical taxi rides',
-    primary_key=['pickup_location_id', 'pickup_hour'],
-    event_time='pickup_hour',
-)
 
+# Feature view name and version
 FEATURE_VIEW_NAME = 'time_series_hourly_feature_view'
 FEATURE_VIEW_VERSION = 1
-FEATURE_VIEW_METADATA = FeatureViewConfig(
-    name='time_series_hourly_feature_view',
-    version=1,
-    feature_group=FEATURE_GROUP_METADATA,
-)
 
+# Model naming
 MODEL_NAME = "yellow_taxi_demand_predictor_next_hour"
 
-MAX_MAE = 10.0
+FEATURE_VIEW_MONITORING = 'predictions_vs_actuals_for_monitoring_feature_view'
+
+# Configuration for the number of historical data points required for prediction
+N_FEATURES = 24 * 28  # This equals the number of hours in 28 days
+
+# Configuration for the maximum allowed mean absolute error for the production model
+MAX_MAE = 10.0  # Threshold for the MAE metric
